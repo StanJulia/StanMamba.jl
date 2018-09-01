091eb3281071e210946477f169b392dbb2ada6f7
Rob J Goedman <goedman@mac.com>
2018-09-01 14:50:16 -0400

    Fixed argument type

--- src/utilities/convert_a3d.jl
+++ src/utilities/convert_a3d.jl
@@ -38,7 +38,7 @@ Options 2 through 4 are respectively provided by the packages StanDataFrames, St
 * `res`                       : Draws converted to the specified format.
 ```
 """
-function convert_a3d(a3d_array, cnames, ::Val{:dataframe})
+function convert_a3d(a3d_array, cnames, ::Val{:mambachains})
   snames = [Symbol(cnames[i]) for i in 1: length(cnames)]
   sr = getindex(a3d_array, [1:1:size(a3d_array, 1);], [1:size(a3d_array, 2);], [1:size(a3d_array, 3);])
   Chains(sr, 1:size(a3d_array, 2), cnames, [i for i in 1:size(a3d_array, 3)])
