7a4e1d9125e1873e17e8d5ab08894eaf731e198e
Rob J Goedman <goedman@mac.com>
2018-10-22 13:54:10 -0600

    Updates for v4.0.0

--- src/StanMamba.jl
+++ src/StanMamba.jl
@@ -6,16 +6,7 @@ using Statistics, Documenter
 import CmdStan: convert_a3d
 #import Mamba: AbstractChains, Chains
 
-
-abstract type AbstractChains end
-
-struct Chains <: AbstractChains
-  value::Array{Float64, 3}
-  range::UnitRange{Int}
-  names::Vector{AbstractString}
-  chains::Vector{Int}
-end
-
+import Mamba: Chains
 
 include("utilities/convert_a3d.jl")
 
