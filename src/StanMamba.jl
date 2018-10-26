07b397dc7373ce4fad3c2f7f677ecc00bf38f13a
Rob J Goedman <goedman@mac.com>
2018-10-26 08:13:23 -0600

    Convert after stan() has completed. Removed Gadfly dependency.

--- src/StanMamba.jl
+++ src/StanMamba.jl
@@ -4,18 +4,7 @@ module StanMamba
 using Statistics, Documenter
 
 import CmdStan: convert_a3d
-#import Mamba: AbstractChains, Chains
-
-
-abstract type AbstractChains end
-
-struct Chains <: AbstractChains
-  value::Array{Float64, 3}
-  range::UnitRange{Int}
-  names::Vector{AbstractString}
-  chains::Vector{Int}
-end
-
+import Mamba: AbstractChains, Chains
 
 include("utilities/convert_a3d.jl")
 
